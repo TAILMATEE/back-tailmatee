81358dd08ce79c1eb8fc79f786d0fde816b4c67b
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-07-05 02:05:25 -0600

    Adding: generate-jwt, middleware validate JWT and sign Secret Key in ENV

---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,54 @@
+import jwt from 'jsonwebtoken';
+import TailUser from '../src/tailUser/tailUser.model.js';
+
+export const validateJWT = async (req, res, next) => {
+    const token = req.header("Authorization");
+
+    if (!token) {
+        return res.status(401).json({
+            msg: "There is no token in the request",
+        });
+    }
+
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETPRIVATEKEY);
+
+        const tailUser = await TailUser.findById(uid);
+
+        if (!tailUser) {
+            return res.status(401).json({
+                msg: 'Tail User does not exist in the database'
+            });
+        }
+
+        switch (tailUser.status) {
+
+            case 'inactive':
+
+                return res.status(401).json({
+                    msg: 'Token is not valid - user with state: INACTIVE'
+                });
+
+
+            case 'blocked':
+
+                return res.status(401).json({
+                    msg: 'Token is not valid - user with state: LOCKED'
+                });
+
+            default:
+
+                break;
+
+        }
+
+        req.tailUser = tailUser;
+
+        next();
+    } catch (e) {
+        console.log(e);
+        res.status(401).json({
+            msg: "Token is not valid",
+        });
+    }
+}
