e2ce637d9f7b66f950473d718cfcec73e62e403d
Kenneth Velásquez <kvelasquez-2019504@kinal.edu.gt>
2024-07-19 03:19:27 -0600

    Addedd function finally tailHouse

--- src/TailHouse/tailHouse.routes.js
+++ src/TailHouse/tailHouse.routes.js
@@ -1,10 +1,8 @@
 import {Router} from 'express';
-import { check } from 'express-validator';
 
 import {validateJWT} from '../middlewares/validate-jwt.js';
-import {validateFields, validateIdTailHouseParam} from '../middlewares/validate-fields.js';
+import {validateIdTailHouseParam} from '../middlewares/validate-fields.js';
 import { getTailHouseById, getTailHouses, putTailHouse } from './tailHouse.controller.js';
-import { validateTailHouse } from '../helpers/validate-db.js';
 import { haveRol } from '../middlewares/validate-role.js'
 
 const router = Router();
